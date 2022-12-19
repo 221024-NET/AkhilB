c3d6fe408bfe083c3a6156f7c6c2512495fa8564
AkhilBh97 <akhil416@revature.net>
2022-12-19 14:11:17 -0800

    started seeding the tables with Franchise info

---
+++ 221024_batch/w9/2022_12_19_SQL/dataseed.sql
@@ -0,0 +1,23 @@
+--Let's start with a few franchises
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'McDonald'+char(39)+'s'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Panda Express'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Olive Garden'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Raising Cane'+char(39)+'s'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Taco Bell'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Buca di Beppo'
+);
+INSERT INTO RESTAURANTS.Franchise VALUES(
+    'Red Lobster'
+);
+GO
